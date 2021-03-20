1c4d5728f14311ede57612d1ca6f5d53c4fd3a90
junyoung44 <44jungyoun44@gmail.com>
2021-03-20 16:35:33 +0900

    메인 페이지
    프로필 이미지//왜안될까?
    이름/나이/학교 소개

--- character_page/lib/main.dart
+++ character_page/lib/main.dart
@@ -7,107 +7,126 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title:'junyoung page',
+      debugShowCheckedModeBanner: false, //debug 그거 지우기
+      home:Mypage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Mypage extends StatelessWidget{
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      backgroundColor: Colors.amber[800],
+        appBar: AppBar(
+            title:Text('Junyoung'), //appbar에 text 넣기
+            centerTitle: true, //appbar에서 title 수정하기
+            backgroundColor: Colors.amber[700], //appbar 색상 수정하기
+            elevation:20.0 //appbar가 떠있는 효과를 준다
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        body:Padding(
+          padding :EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //저번에 main AxisAlignment 쓴거랑 다르게 가로축 정렬을 해줌
+            // start는 왼쪽 정렬!
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('resource/cat.gif'),
+                  radius: 60.0,//크기 조정
+                  backgroundColor: Colors.white, //투
+                ),
+              ), //아주 멋진 위젯
+              Divider( //구분선
+                height: 60.0, //이거는 위와 아래를 30씩만큼 나눈다는것 (위에 30, 아래에 30)
+                color: Colors.grey[850], //색
+                thickness: 0.5, //굵기
+                endIndent: 0.0, //끝에서부터 얼마나 떨어져있는지 (패딩이 30이니까 똑같이 30해주면 된다)
+              ),
+              Text('NAME',
+              style: TextStyle( //text의 style을 지정할 수 있는 위젯이다. ctrl을 누르고 클릭하면 다양한 속성을 볼 수 있다.
+                color:Colors.white, //글자 색깔 바꿔줌
+                letterSpacing: 2.0, //철자 간 간격 조정해줌
+              ),),
+              SizedBox(
+                height:20.0, //위 아래 간격 사이를 10만큼 넓혀준다
+              ),
+              Text('Junyoung',
+              style: TextStyle(
+                color:Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0, //글자 굵기 (단위가 뭐지?)
+                fontWeight: FontWeight.bold, //글자 굵기 조절해줌
+              ),),
+              SizedBox(
+                height:30.0, //위 아래 간격 사이를 10만큼 넓혀준다
+              ),
+              Text('LEVEL',
+                style: TextStyle( //text의 style을 지정할 수 있는 위젯이다. ctrl을 누르고 클릭하면 다양한 속성을 볼 수 있다.
+                  color:Colors.white, //글자 색깔 바꿔줌
+                  letterSpacing: 2.0, //철자 간 간격 조정해줌
+                ),),
+              SizedBox(
+                height:10.0, //위 아래 간격 사이를 10만큼 넓혀준다
+              ),
+              Text('22',
+                style: TextStyle(
+                  color:Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0, //글자 굵기 (단위가 뭐지?)
+                  fontWeight: FontWeight.bold, //글자 굵기 조절해줌
+                ),
+              ),
+              SizedBox(
+                height:40.0, //위 아래 간격 사이를 10만큼 넓혀준다
+              ),
+              Row(
+                children:<Widget>[
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width:10,
+                  ),
+                  Text('Computer Education',
+                  style:TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),)
+                ]
+              ),
+              Row(
+                  children:<Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width:10,
+                    ),
+                    Text('College of Education',
+                      style:TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0,
+                      ),)
+                  ]
+              ),
+              Row(
+                  children:<Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width:10,
+                    ),
+                    Text('Sungkyungwan University',
+                      style:TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0,
+                      ),)
+                  ]
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
